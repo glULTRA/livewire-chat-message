2f4bb63bd9694149a9b517cb79dffafeaffb4c28
glULTRA <7amakurd2002@gmail.com>
2022-12-29 14:41:02 +0300

    Pusher has been setup

--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -1,9 +1,9 @@
 <div>
     {{-- Stop trying to control. --}}
 
-    <ul class="list-group w-75 mx-auto mt-3 container-fluid text-white">
+    <ul class="list-group px-3 w-10/12 mx-auto mt-3 container-fluid text-white">
         @foreach ($users as $user)
-        <li class="list-group-item list-group-item-action mt-3 dark:bg-slate-800 dark:text-white" wire:click='checkconversation({{$user->id}})'>
+        <li class="list-group-item p-2 dark:shadow-lg dark:shadow-black rounded-t-lg text-center cursor-pointer list-group-item-action mt-3 dark:bg-slate-800 dark:text-white" wire:click='checkconversation({{$user->id}})'>
             {{$user->name}}
         </li>
         @endforeach
